ae110ffb071944ea52d3c80044f122ac52e235e1
IlemiOnline <ilemionline@MacBook-de-IlemiOnline.local>
2021-01-23 12:53:45 -0600

    fiex app state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
 // --------------------------------------
 // Imports
 // --------------------------------------
-  import { useState, useRef } from 'react';  
+  import { useState, useEffect, useRef } from 'react';  
   import {Player, Song, Library, Nav } from './components';
   import data from "./util";
   import "./styles/app.scss";
@@ -26,20 +26,23 @@
     // ?--------------------------------------
     
     
-    // const [songs, setSongs] = useState(data());
-    // setSongs(data());
-    const songs = data();
-    const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [songs, setSongs] = useState([]);
+    const [currentSong, setCurrentSong] = useState(data[0]);
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(null);
     const [libraryStatus, setLibraryStatus] = useState(false);
+
+    useEffect(() => {
+      setSongs(data);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
     return (
       <div className={`App ${libraryStatus ? 'library-active' : ''}`}>
         <Nav libraryStatus = {libraryStatus} setLibraryStatus = {setLibraryStatus}/>
         <Song currentSong = {currentSong} />
         <Player currentSong = {currentSong} isPlaying = {isPlaying} setIsPlaying = {setIsPlaying} audioRef = {audioRef} songs={songs} setCurrentSong = {setCurrentSong} />
-        <Library library =  {songs} setCurrentSong = {setCurrentSong} audioRef = {audioRef} activeSong = {currentSong.id} libraryStatus = {libraryStatus} />
+        <Library library =  {songs} setCurrentSong = {setCurrentSong} audioRef = {audioRef} activeSong = {currentSong && currentSong.id} libraryStatus = {libraryStatus} />
       </div>
     );
   }
